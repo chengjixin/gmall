69199fb948b636146d49c8c0e48f12d012058315
tom_glb <goodMorning@atguigu.com>
2020-09-06 21:43:42 +0800

    thymeleaf的使用说明

--- redirect/src/main/java/com/lenovo/hawk/redirect/controller/RedirectController.java
+++ redirect/src/main/java/com/lenovo/hawk/redirect/controller/RedirectController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
+/**
+ * 我们使用 thymeleaf 的时候,需要注意的是,返回页面不能使用restController 和 responseBody,
+ * 并且thymeleaf页面跳转只能通过控制器来实现
+ */
 @Controller
 public class RedirectController {
     @RequestMapping(value = "/loginPage",method = RequestMethod.GET)
